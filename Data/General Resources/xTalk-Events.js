c96c58af8cf0a071242e78f85571846f736f0fdd
sanathw <sanathw@hotmail.com>
2020-08-11 19:19:50 +1000

    Z

--- Data/General Resources/xTalk-Events.js
+++ Data/General Resources/xTalk-Events.js
@@ -23,16 +23,16 @@ addxTalk("P", "Compilers", "Under the covers with C#: From code to assembly", "
     "James_Venning.png", //Photo
     "2016-12-07%20Compilers%20%28by%20Jamie%20Venning%29", // Talk Folder
     "C# Deep-dive: What does your code actually become?", // Description
-    "https://drive.google.com/file/d/1WlkGWkd27yUXhIdZEKP8lvk4tGaQCrvT/view?usp=sharing", // Google Folder Link
-    "https://drive.google.com/drive/folders/18vEtn2YhZEDCG5YmxFc6v8JtQVmMR_8i?usp=sharing"); // Google Video Link
+    "https://drive.google.com/drive/folders/18vEtn2YhZEDCG5YmxFc6v8JtQVmMR_8i?usp=sharing", // Google Folder Link
+    "https://drive.google.com/file/d/1WlkGWkd27yUXhIdZEKP8lvk4tGaQCrvT/view?usp=sharing"); // Google Video Link
 
 //addxTalk("F", "Two Slit Experiment", "The Two Slit Experiment - Quantum Physics and Consciousness", "Sanath Wanigaratne", 2016, 11, 9, 12, 15, 13, 0, "TR3", "Sanath_Wanigaratne.png", "", "", "", "");
 addxTalk("P", "Cool Algorithms", "Cool Algorithms - Evolutionary AI and Autonomous Motion Planning", "Scott Gibbs", 2016, 11, 14, 12, 15, 13, 0, "TR3", 
     "Scott_Gibbs.png", //Photo
     "2016-11-14%20Cool%20Algorithms%20%28by%20Scott%20Gibbs%29", // Talk Folder
     "Computers simulating intelligent behaviour....come and discover the beauty behind computer intelligence", // Description
-    "https://drive.google.com/open?id=1lDmvDz-UsP3BCgisVcWoHgtVYzdZX53P", // Google Folder Link
-    "https://drive.google.com/file/d/1XieKRqI3swkwYDp4r8FnGpuE_-EAhRmf/view"); // Google Video Link
+    "https://drive.google.com/drive/folders/1lDmvDz-UsP3BCgisVcWoHgtVYzdZX53P?usp=sharing", // Google Folder Link
+    "https://drive.google.com/file/d/1XieKRqI3swkwYDp4r8FnGpuE_-EAhRmf/view?usp=sharing"); // Google Video Link
 
 //addxTalk("F", "Entanglement", "Quantum Entanglement - Welcome to the Matrix", "Sanath Wanigaratne", 2016, 10, 12, 12, 15, 13, 0, "TR3", "Sanath_Wanigaratne.png", "", "", "", "");
 //addxTalk("F", "Two Slit Experiment", "The Two Slit Experiment - Quantum Physics and Consciousness", "Sanath Wanigaratne", 2016, 10, 12, 12, 15, 13, 0, "TR3", "Sanath_Wanigaratne.png", "", "", "", "");
@@ -42,16 +42,16 @@ addxTalk("P", "Stellar", "Stellar Astronomy", "Mark Gasiorowski", 2016, 10, 26, 
     "Mark_Gasiorowski.png", //Photo
     "2016-10-26%20Stellar%20Astronomy%20%28by%20Mark%20Gasiorowski%29", // Talk Folder
     "Learn about how stars are made, how they work, and why you're made of them.", // Description
-    "https://drive.google.com/open?id=10ywuk3RAjOUvi7XjZ_tXO80QW8CpenQ1", // Google Folder Link
-    "https://drive.google.com/file/d/18w649rHpEHbt_Hdg96DILVRDDrtQNx6c/view"); // Google Video Link
+    "https://drive.google.com/drive/folders/10ywuk3RAjOUvi7XjZ_tXO80QW8CpenQ1?usp=sharing", // Google Folder Link
+    "https://drive.google.com/file/d/18w649rHpEHbt_Hdg96DILVRDDrtQNx6c/view?usp=sharing"); // Google Video Link
 
 //addxTalk("N", "Sign Language", "Introduction to Auslan & Deaf Culture", "Stephanie Soya", 2016, 8, 3, 12, 15, 13, 0, "TR3", 
 addxTalk("P", "Sign Language", "Introduction to Auslan & Deaf Culture", "Stephanie Soya", 2016, 9, 14, 12, 15, 13, 0, "TR3", 
     "Stephanie_Soya.png", // Photo
     "2016-09-14%20Auslan%20%28by%20Stephanie%20Soya%29", // Talk Folder
     "Explore the world of Australian Sign Language (Auslan) and learn more about intriguing facets of the Deaf community.", // Description 
-    "https://drive.google.com/open?id=1ikwO54NmFl983Wpt0iFkNZaZrqmIPlgF", // Google Folder Link
-    "https://drive.google.com/file/d/1BRLvTmAMjv4VxBpH1GvcA2QVrO6QP_j8/view"); // Google Video Link
+    "https://drive.google.com/drive/folders/1ikwO54NmFl983Wpt0iFkNZaZrqmIPlgF?usp=sharing", // Google Folder Link
+    "https://drive.google.com/file/d/1BRLvTmAMjv4VxBpH1GvcA2QVrO6QP_j8/view?usp=sharing"); // Google Video Link
 
 //addxTalk("N", "Xamarin", "Xamarin -  iOS & Android Apps in C#", "Julian Marshall", 2016, 7, 13, 12, 15, 13, 0, "TR3",
 //    "Julian_Marshall.png",  // Photo
@@ -69,58 +69,58 @@ addxTalk("P", "Entanglement", "Quantum Entanglement - Welcome to the Matrix", "S
     "Sanath_Wanigaratne.png",  // Photo
     "2016-07-13%20Quantum%20Entanglement%20%28by%20Sanath%20Wanigaratne%29",  // Talk Folder
     "Quantum Physics is really, really strange. Quantum Physics is really, really true. Come find out why.", // Description 
-    "https://drive.google.com/open?id=1OGCsWkhpHnFTYF3jK3Ft6v-2z93pvF64", // Google Folder Link
-    "https://drive.google.com/file/d/1kXDGJCVBnPcfG0wFyuVmhtet5fUqtDjY/view"); // Google Video Link
+    "https://drive.google.com/drive/folders/1OGCsWkhpHnFTYF3jK3Ft6v-2z93pvF64?usp=sharing", // Google Folder Link
+    "https://drive.google.com/file/d/1kXDGJCVBnPcfG0wFyuVmhtet5fUqtDjY/view?usp=sharing"); // Google Video Link
 
 addxTalk("P", "Singularity", "Singularity / Basic Income / Technological unemployment", "David Kelertas", 2016, 6, 8, 12, 15, 13, 0, "TR3", 
     "David_Kelertas.png", // Photo
     "2016-06-08%20Singularity%20%28by%20David%20Kelertas%29", // Talk Folder
     "What is the singularity? When will it occur? What will it mean to humans? Technological unemployment has been happening for centuries, are we now nearing an endpoint to human employment? What will we do without employment income?", // Description 
-    "https://drive.google.com/open?id=1eNEmosAyktVUTSj92ub-rngKWzn5FgXm", // Google Folder Link
-    "https://drive.google.com/file/d/1G7vKiACDoYdf9n8cfb3pirmzf48sOfm2/view"); // Google Video Link
+    "https://drive.google.com/drive/folders/1eNEmosAyktVUTSj92ub-rngKWzn5FgXm?usp=sharing", // Google Folder Link
+    "https://drive.google.com/file/d/1G7vKiACDoYdf9n8cfb3pirmzf48sOfm2/view?usp=sharing"); // Google Video Link
 
 addxTalk("P", "Myers Briggs", "Myers Briggs Personality Test - What's your Barcode?", "Sanath Wanigaratne", 2016, 5, 19, 12, 15, 13, 0, "TR4", 
     "Sanath_Wanigaratne.png", // Photo
     "2016-05-19%20Myers%20Briggs%20Personality%20Test%20-%20What%27s%20your%20Barcode%20%28by%20Sanath%20Wanigaratne%29", // Talk Folder
     "Find out what your MBTI code is and what it means. Learn about the history, implications and use of this method from a philosophical view point.", // Description
-    "https://drive.google.com/open?id=1pFLnRd_4dfs1EzojhwYEuvL03UlBuFZh",  // Google Folder Link 
+    "https://drive.google.com/drive/folders/1pFLnRd_4dfs1EzojhwYEuvL03UlBuFZh?usp=sharing",  // Google Folder Link 
     "https://drive.google.com/file/d/1EdIvx6q9moYzqErdEhHvQh-WklT5eL9c/view?usp=sharing"); // Google Video Link
 
 addxTalk("P", "Astronomy", "An Intro to Amateur Astronomy in Brisbane", "Mark Gasiorowski", 2016, 4, 20, 12, 15, 13, 0, "TR4", 
     "Mark_Gasiorowski.png", // Photo
     "2016-04-20%20An%20Intro%20to%20Amateur%20Astronomy%20in%20Brisbane%20%28by%20Mark%20Gasiorowski%29", // Talk Folder
     "Learn about types and cost of telescopes, what/where/how to observe, clubs and other information about amateur astronomy in Brisbane.", // Description
-    "https://drive.google.com/open?id=1QtvcH2vnH9Bi1N2SJ_C32Ra4T8Aeif0Y",  // Google Folder Link
-    "https://drive.google.com/file/d/1-6vCndV0wvz-BloPtPwGmYRZ3P9ht9YS/view"); // Google Video Link
+    "https://drive.google.com/drive/folders/1QtvcH2vnH9Bi1N2SJ_C32Ra4T8Aeif0Y?usp=sharing",  // Google Folder Link
+    "https://drive.google.com/file/d/1-6vCndV0wvz-BloPtPwGmYRZ3P9ht9YS/view?usp=sharing"); // Google Video Link
 
 addxTalk("P", "Crucial Conversations", "Crucial Conversations", "Julian Marshall", 2016, 3, 16, 12, 15, 13, 0, "TR3", 
     "Julian_Marshall.png", // Photo
     "2016-03-16%20Crucial%20Conversations%20%28by%20Julian%20Marshall%29", // Talk Folder
     "Find out how to communicate better and notice which aspects of conversations you are in.", // Description
-    "https://drive.google.com/open?id=1FsWJCZZOSjMAKnMkjwNV0681COomnutf",  // Google Folder Link
-    "https://drive.google.com/file/d/1ysyWx6h6SziHnhSL-q8fYWNyo0wUxDha/view"); // Google Video Link
+    "https://drive.google.com/drive/folders/1FsWJCZZOSjMAKnMkjwNV0681COomnutf?usp=sharing",  // Google Folder Link
+    "https://drive.google.com/file/d/1ysyWx6h6SziHnhSL-q8fYWNyo0wUxDha/view?usp=sharing"); // Google Video Link
 
 addxTalk("P", "Evolution", "Evolution<br>...by Design", "Sanath Wanigaratne", 2016, 3, 02, 12, 15, 13, 0, "TR3", 
     "Sanath_Wanigaratne.png", // Photo
     "2016-03-02%20Evolution...by%20Design%20%28by%20Sanath%20Wanigaratne%29", // Talk Folder
     "Nature inspired algorithms that can solve problems without us putting any effort.", // Description
-    "https://drive.google.com/open?id=1EuMNJMAeQkR7mwE-giG2YzbCnUF73HYu",  // Google Folder Link
-    "https://drive.google.com/file/d/1SQczjzCKNnHoYRwOGxdQjhCPqkLFnjGG/view"); // Google Video Link
+    "https://drive.google.com/drive/folders/1EuMNJMAeQkR7mwE-giG2YzbCnUF73HYu?usp=sharing",  // Google Folder Link
+    "https://drive.google.com/file/d/1SQczjzCKNnHoYRwOGxdQjhCPqkLFnjGG/view?usp=sharing"); // Google Video Link
 
 addxTalk("P", "Sierpinski Triangle", "The Sierpinski Triangle<br>&  Interfacing iTunes with SoundCloud", "Sanath Wanigaratne<br>& Joshua O’Brien", 2016, 2, 09, 12, 15, 13, 0, "TR3", 
     "Sanath_Wanigaratne.png", // Photo
     "2016-02-09%20The%20Sierpinski%20Triangle%20%26%20%20Interfacing%20iTunes%20with%20SoundCloud%20%28by%20Sanath%20Wanigaratne%20%26%20Joshua%20O%E2%80%99Brien%29", // Talk Folder
     "See how randomness and chaos can create order!", // Description
-    "https://drive.google.com/open?id=1NHhU__Qlo77gfjo95O69DvDKqcVO7_Bu",  // Google Folder Link
-    "https://drive.google.com/file/d/1wA5s5gPIAixZV7NVrOYQhwtEdiHLLdnZ/view"); // Google Video Link
+    "https://drive.google.com/drive/folders/1NHhU__Qlo77gfjo95O69DvDKqcVO7_Bu?usp=sharing",  // Google Folder Link
+    "https://drive.google.com/file/d/1wA5s5gPIAixZV7NVrOYQhwtEdiHLLdnZ/view?usp=sharing"); // Google Video Link
 
 
 addxTalk("P", "Memory", "Memory Techniques - The real TOTAL RECALL", "Sanath Wanigaratne", 2015, 12, 03, 12, 15, 13, 0, "TR3", 
     "Sanath_Wanigaratne.png", // Photo
     "2015-12-03%20Memory%20Techniques%20-%20The%20real%20TOTAL%20RECALL%20%28by%20Sanath%20Wanigaratne%29", // Talk Folder
     "Discover how to memorise an incredible amount of things with ease!", // Description
-    "https://drive.google.com/open?id=1-QggqDNpZnm0o9XHprtppAGWT4Uuz-wg",  // Google Folder Link
-    "https://drive.google.com/file/d/1kgzs9Pf91FMs6GJckO6LxZzynEbUBLJi/view"); // Google Video Link
+    "https://drive.google.com/drive/folders/1-QggqDNpZnm0o9XHprtppAGWT4Uuz-wg?usp=sharing",  // Google Folder Link
+    "https://drive.google.com/file/d/1BgjaMdlniv4TFoENFrySZuPMs90r1BC0/view?usp=sharing"); // Google Video Link
 
 
 addxTalk("P", "Magic", "Magic - Unleash your mind's potential", "Sanath Wanigaratne", 2015, 11, 20, 12, 15, 13, 0, "TR3", 
@@ -135,7 +135,7 @@ addxTalk("P", "First", "Quantum Physics", "Sanath Wanigaratne", 2015, 10, 20, 12
     "Sanath_Wanigaratne.png", // Photo
     "", // Talk Folder
     "Demonstration and explanation of Quantum Entanglement.", // Description
-    "https://drive.google.com/open?id=0B3HsIsgdcWNCa3N5LWRCV2F5OWs",  // Google Folder Link
+    "https://drive.google.com/drive/folders/1t97_bpbxww0yNLgYiBpUsCTFywI9bgyQ?usp=sharing",  // Google Folder Link
     ""); // Google Video Link
     
     
